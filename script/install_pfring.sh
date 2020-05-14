b91b5f149ccb8d48eea507b6082fcc70732ed44c
Xumeiquer <jaumartin@gmail.com>
2020-05-14 23:28:47 +0200

    Update travis.yml

--- script/install_pfring.sh
+++ script/install_pfring.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
+cwd=$(pwd)
+
+cd /tmp
+
 git clone https://github.com/ntop/PF_RING.git
 cd PF_RING/kernel/
 make && sudo make install
+
 cd ../userland/lib
 ./configure --prefix=/usr/local/pfring && make && sudo make install
+
 cd ../libpcap
 ./configure --prefix=/usr/local/pfring && make && sudo make install
+
 cd ../tcpdump
 ./configure --prefix=/usr/local/pfring && make && sudo make install
+
 sudo ldconfig
 export CGO_LDFLAGS="-L/usr/local/pfring/lib/"
 export CGO_CFLAGS="-I/usr/local/pfring/include/"
 
 sudo modprobe pf_ring
 echo pf_ring | sudo tee -a /etc/modules
+
+cd "$pwd"
--- script/install_yara.sh
+++ script/install_yara.sh
@@ -2,11 +2,16 @@
 
 YARA_VERSION="3.11.0"
 
+cwd=$(pwd)
+cd /tmp
+
 wget "https://github.com/VirusTotal/yara/archive/v$YARA_VERSION.tar.gz" -O yara.tgz
 tar xvfz yara.tgz
 cd yara-$YARA_VERSION
 
 ./bootstrap.sh
-./configure
+./configure --enable-magic --with-crypto
 make
 sudo make install
+
+cd $cwd
