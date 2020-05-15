b0dfa9ab8540c6812abadf9b5b9fb950d587c514
Xumeiquer <jaumartin@gmail.com>
2020-05-15 17:08:35 +0200

    Update travis.yml

---
+++ script/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if ! [ "$BEFORE_DEPLOY_RUN" ]; then
+    export BEFORE_DEPLOY_RUN=1;
+
+    if [ x$TRAVIS_TAG != x"" ]; then
+        echo "Building binaries";
+        make build;
+        cd build;
+        sha256sum -b * > $(ls *)".sha256";
+        cd -;
+    fi;
+
+    echo "Download documentation generator";
+    curl -sfL https://raw.githubusercontent.com/containous/structor/master/godownloader.sh | bash -s -- -b $GOPATH/bin ${STRUCTOR_VERSION};
+    
+    echo "Build documentation";
+    sudo -E "$GOPATH/bin/structor" -o mole-ids -r mole \
+            --force-edit-url \
+            --dockerfile-url="https://raw.githubusercontent.com/mole-ids/mole/master/docs/docs.Dockerfile" \
+            --menu.js-url="https://raw.githubusercontent.com/mole-ids/mole/master/docs/theme/structor-menu.js.gotmpl" \
+            --exp-branch=master --debug;
+    sudo -E chown -R $UID site;
+fi
