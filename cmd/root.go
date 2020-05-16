e9ad4ca19049a8c4c2086ae9003c4d6ac4322a68
Xumeiquer <jaumartin@gmail.com>
2020-05-16 13:03:20 +0200

    Added Apache 2.0 license

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,16 @@
+// Copyright 2020 Jaume Martin
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package cmd
 
 import (
@@ -11,8 +24,8 @@ import (
 )
 
 var (
-	AppName = "Mole IDS"
-	Version = "v0.0.0"
+	AppName   = "Mole IDS"
+	Version   = "v0.0.0"
 	BuildHash = "devel"
 	BuildDate = "1970-01-01T00:00:00Z"
 )
@@ -48,7 +61,7 @@ func init() {
 	RootCmd.PersistentFlags().String("logLevel", "info", "Log level")
 
 	RootCmd.PersistentFlags().Bool("version", false, "Show Mole version")
-	
+
 	// Bind flags to configuration file
 	viper.BindPFlag("logger.log_to", RootCmd.PersistentFlags().Lookup("logTo"))
 	viper.BindPFlag("logger.log_level", RootCmd.PersistentFlags().Lookup("logLevel"))
